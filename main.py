b35027cbce024f2cf492ac22f2010e1c7f72a836
Your Name <you@example.com>
2026-09-15 15:20:30 +0300

    Update main.py with examples

---
+++ main.py
@@ -0,0 +1,31 @@
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+
+f = fibonacci()
+for i in range(10):
+    print(next(f))
+
+# => 0
+# => 1
+# => 1
+# => 2
+# => 3
+# => 5
+# => 8
+# => 13
+# => 21
+# => 34
+
+
+
+
+
+
+
+
+
